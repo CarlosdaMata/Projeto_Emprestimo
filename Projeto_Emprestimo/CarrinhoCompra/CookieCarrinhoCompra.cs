d6e12ee47d11d65a0ebfb019d30bec8e41f25369
jamaral33 <jpgamedev07@gmail.com>
2026-04-29 09:25:36 -0300

    pagina 20 do pdf

---
+++ Projeto_Emprestimo/CarrinhoCompra/CookieCarrinhoCompra.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto_Emprestimo.CarrinhoCompra
+{
+    //criando chave
+    private string Key = "Carrinho.Compras";
+    private Cookie.manipCookie _cookie;
+    public CookieCarrinhoCompra(Cookie.manipCookie cookie)
+    {
+        _cookie = cookie;
+    }
+}
